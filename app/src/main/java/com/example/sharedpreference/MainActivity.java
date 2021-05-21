1a310a77126640e5e05c23c51041153828650849
mits <devanshu18g@gmail.com>
2021-05-21 15:06:58 +0530

    second commit

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name,age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.edit1);
+        age =  findViewById(R.id.edit2);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+
+
+
     }
 }
